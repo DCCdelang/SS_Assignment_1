3e1b008b27b13a16f48368406d141dbba8f7a570
Karim-1 <54839428+Karim-1@users.noreply.github.com>
2020-10-30 12:53:24 +0100

    updates

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 # from numba import 
 
@@ -12,33 +11,4 @@ def mandelbrot(real_nr, imag_nr, max_iter):
         if (z.real * z.real + z.imag * z.imag) >= 4:
             return i
     return max_iter
-
-# resolution
-rows, cols = 500, 500
-
-
-result = np.zeros([rows, cols])
-
-for real_index, Re in enumerate(np.linspace(-2.25, .75, num=rows )):
-    for imag_index, Im in enumerate(np.linspace(-1.5, 1.5, num=cols)):
-        result[real_index, imag_index] = mandelbrot(Re, Im, 100)
-
-area = np.count_nonzero(result)
-
-pixelated_proportion = area / (rows*cols)
-print('Proportion of image that contains colored pixels = ', pixelated_proportion)
-avg_pixel_value = np.mean(result)/100
-print('Average value per pixel = ', np.mean(result)/100)
-total_area = 3*3
-print('Total area = 3 * 2 =', total_area)
-
-print('Total area of mandelbrot =', pixelated_proportion * avg_pixel_value * total_area)
-
-
-plt.figure(dpi=100)
-plt.imshow(result.T, extent=[-2, 1, -1, 1])
-plt.xlabel('real numbers')
-plt.ylabel('imaginary numbers')
-plt.show()
-
 
